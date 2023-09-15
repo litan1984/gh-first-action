1ebe1e605cf386d94adc0d654dc410e83c25edef
Litan Behera <litanbehera@Litans-MacBook-Air.local>
2023-09-15 19:53:26 +0530

    first terraform code

--- Azure.Resources/TF_GitHub/azure-resource-group.tf
+++ Azure.Resources/TF_GitHub/azure-resource-group.tf
@@ -16,6 +16,10 @@ provider "azurerm" {
   # client_id       = "d8c4b7d6-eeb9-444e-b711-aa37d84b89ea"
   # client_secret   = "yUg8Q~P~JF_DAO5oU~BEaTmyBRWT~pOUlKeAAaqZ"
   # tenant_id       = "bc540cd6-65a2-493d-8ec5-ee06f9bbb60e"
+  subscription_id = "4b09b3ee-592e-48fd-a985-cb26811b0967"
+  client_id       = "8b5273fc-e753-41cb-a3e0-a2ec8ece9a79"
+  tenant_id       = "bc540cd6-65a2-493d-8ec5-ee06f9bbb60e"
+  use_oidc = true
 }
 
 # Create Resource Group 
