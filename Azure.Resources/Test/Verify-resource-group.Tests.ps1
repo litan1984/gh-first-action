3e66d7ce4a94024bd439228f7f86014fc88ba4b1
Litan Behera <litanbehera@Litans-MacBook-Air.local>
2023-09-15 21:23:40 +0530

    first terraform code

--- Azure.Resources/Test/Verify-resource-group.Tests.ps1
+++ Azure.Resources/Test/Verify-resource-group.Tests.ps1
@@ -1,11 +1,11 @@
 $resourceGroup = Get-AzResourceGroup
 Write-Output "Resource group list: $resourceGroup"
 
-Describe 'Azure resources'{
-    Context 'Azure resource group'{
-        It 'Verify resource group'{
-            $resourceGroup[1].ResourceGroupName | Should -Be "my-demo-rg1"
-        }
+# Describe 'Azure resources'{
+#     Context 'Azure resource group'{
+#         It 'Verify resource group'{
+#             $resourceGroup[1].ResourceGroupName | Should -Be "my-demo-rg1"
+#         }
         
-    }
-}
+#     }
+# }
