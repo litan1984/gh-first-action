bd33890fcfc7e663238ddd6bc04060412aa0e574
Litan Behera <litanbehera@Litans-MacBook-Air.local>
2023-10-01 15:44:25 +0530

    Docker test

--- Azure.Resources/TF_GitHub/azure-resource-group.tf
+++ Azure.Resources/TF_GitHub/azure-resource-group.tf
@@ -29,7 +29,7 @@ resource "azurerm_resource_group" "my_demo_rg1" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "containerRegistry1"
+  name                = "azcr25041984"
   resource_group_name = azurerm_resource_group.my_demo_rg1.name
   location            = azurerm_resource_group.my_demo_rg1.location
   sku                 = "Premium"
