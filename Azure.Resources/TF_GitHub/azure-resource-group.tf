548fac122b154c6cae073850d2d491e0db03fc7d
Litan Behera <litanbehera@Litans-MacBook-Air.local>
2023-10-02 20:14:25 +0530

    terraform init

--- Azure.Resources/TF_GitHub/azure-resource-group.tf
+++ Azure.Resources/TF_GitHub/azure-resource-group.tf
@@ -16,7 +16,7 @@ resource "azurerm_storage_account" "tfstate" {
   name = "asa${random_string.myrandom.id}"
   resource_group_name = azurerm_resource_group.my_demo_rg1.name
   location = azurerm_resource_group.my_demo_rg1.location
-  access_tier = "Standard"
+  account_tier = "Standard"
   account_replication_type = "LRS"
   allow_nested_items_to_be_public = true
   tags = {
@@ -27,7 +27,7 @@ resource "azurerm_storage_account" "tfstate" {
 resource "azurerm_storage_container" "tfstate" {
   name = "tfstate"
   storage_storage_account_name = azurerm_storage_account.tfstate.name
-  contaicontainer_access_type = "private"  
+  container_access_type = "private"  
 }
 
 resource "random_string" "myrandom" {
